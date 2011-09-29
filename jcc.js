227d9bfa9361312d80d5b5ef1dbd3ce31b769251
Dvd848 <Dvd848@gmail.com>
2011-09-29 13:09:05 +0300

    Change-Id: If2b6b256424c7fd395e356d42ddff3eebf1e588f

--- jcc.js
+++ jcc.js
@@ -19,7 +19,6 @@ function printDebug(msg) {
  
  */
 
-
 /*
  * A structure representing a jcc-related bundle of elements.
  * A new bundle is created for every jcc parent node (a node with class="jcc"), 
